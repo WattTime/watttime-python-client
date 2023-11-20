1bc4ef553e8c2a32aa0dfe36bb1e38618ef7320c
Nat Steinsultz <nat.steinsultz@gmail.com>
2023-11-20 13:50:22 -0600

    change python to 3.8+, add dateutil

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ setup(
         WattTime API, including data that spans across the 30 day API request limit.""",
     version="3.0",
     packages=["watttime_sdk"],
-    python_requires=">=3.7",
-    install_requires=[
-        "requests",
-        "pandas>1.0.0"
-    ]
+    python_requires=">=3.8",
+    install_requires=["requests", "pandas>1.0.0", "python-dateutil"],
 )

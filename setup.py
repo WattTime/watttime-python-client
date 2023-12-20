af8db1d5ef536e6d9ca0be1c75d0014a6ace37c2
sam-watttime <sam@watttime.org>
2023-12-20 13:26:59 -0700

    bump setup.py version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description="""
         A python software development kit with basic examples for using the
         WattTime API, including data that spans across the 30 day API request limit.""",
-    version="3.0",
+    version="v1.0",
     packages=["watttime_client"],
     python_requires=">=3.8",
     install_requires=["requests", "pandas>1.0.0", "python-dateutil"],

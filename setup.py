25985103dd911723774edb9d697ba8c22d6956db
sam-watttime <75635755+sam-watttime@users.noreply.github.com>
2024-03-21 13:56:12 -0600

    v1.2 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description="An officially maintained python client for WattTime's API providing access to electricity grid emissions data.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    version="v1.1",
+    version="v1.2",
     packages=["watttime"],
     python_requires=">=3.8",
     install_requires=["requests", "pandas>1.0.0", "python-dateutil"],

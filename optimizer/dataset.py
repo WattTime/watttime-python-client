57f89003d1e693207a5961a5a395110a04334e7c
jbadsdata <jennifer.badolato@gmail.com>
2024-06-14 14:43:41 +0000

    historical

--- optimizer/dataset.py
+++ optimizer/dataset.py
@@ -3,13 +3,13 @@ import pandas as pd
 from typing import List
 
 us_regions = pd.read_csv("optimizer/us_region_meta_data.csv",index_col=0)
-us_region_list_forecast = us_regions.query('endpoint == "v3/forecast"')["region"].to_list()
+us_region_list = us_regions.query('endpoint == "v3/forecast"')["region"].to_list()
 
 def get_forecast_multigrid_regions(
     username:str,
     password:str,
     horizon_hours:int = 24,
-    region_list: List[str] = us_region_list_forecast,
+    region_list: List[str] = us_region_list,
     signal_type: str = 'co2_moer',
     include_meta = False
 ):
@@ -25,3 +25,47 @@ def get_forecast_multigrid_regions(
         dfs.append(df_r)
     df = pd.concat(dfs)
     return df
+
+
+def get_historical_multigrid_regions(
+    username:str,
+    password:str,
+    start = '2023-06-01',
+    end = '2024-06-01',
+    region_list: List[str] = us_region_list,
+    signal_type: str = 'co2_moer',
+    include_meta = False
+):
+    wt_hist = WattTimeHistorical(username, password)
+    dfs = []
+    for region in region_list:
+        df_r = wt_hist.get_historical_forecast_pandas(
+            start = start,
+            end = end,
+            region = region,
+            signal_type = signal_type,
+            include_meta = include_meta
+            )
+        df_r["region"] = region
+        dfs.append(df_r)
+    df = pd.concat(dfs)
+    return df
+
+def get_historical_csvs_multigrid_regions(
+    username:str,
+    password:str,
+    start = '2023-06-01',
+    end = '2024-06-01',
+    region_list: List[str] = us_region_list,
+    signal_type: str = 'co2_moer',
+    include_meta = False
+):
+    wt_hist = WattTimeHistorical(username, password)
+    for region in region_list:
+        df_r = wt_hist.get_historical_csv(
+            start = start,
+            end = end,
+            region = region,
+            signal_type = signal_type,
+            include_meta = include_meta
+            )

f445159221356e24e594ff7ffd6eb884ee533483
canyonfoot <canyon.foot@gmail.com>
2024-11-11 16:33:55 +0000

    fix spaces

--- watttime/api.py
+++ watttime/api.py
@@ -613,6 +613,7 @@ class WattTimeOptimizer(WattTimeForecast):
         - It supports various optimization methods and can handle both constant and variable power usage.
         - The resulting plan aims to minimize emissions while meeting the specified energy requirements.
         """
+
         def is_tz_aware(dt):
             return dt.tzinfo is not None and dt.tzinfo.utcoffset(dt) is not None
 
@@ -779,6 +780,7 @@ class RecalculatingWattTimeOptimizer:
         self.total_time_required = usage_time_required_minutes
         self.usage_power_kw = usage_power_kw
         self.optimization_method = optimization_method
+        
         # Setup for us to track schedule/usage
         self.all_schedules = [] # (schedule, ctx)
         self.remaining_time_required = usage_time_required_minutes

59c1d8ccdbb40fd8fb119994a20b60cb02194e57
sam-watttime <sam@watttime.org>
2025-01-21 23:24:33 -0700

    add extras to setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name="watttime",
     description="An officially maintained python client for WattTime's API providing access to electricity grid emissions data.",
-    long_description=open('README.md').read(),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     version="v1.3.2",
     packages=["watttime"],
     python_requires=">=3.8",
     install_requires=["requests", "pandas>1.0.0", "holidays", "python-dateutil"],
+    extras_require={"report": ["papermill", "nbconvert", "plotly", "scipy"]},
 )

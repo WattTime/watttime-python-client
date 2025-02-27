97305149281e01e40322b0b7228df9d1556269ca
samkoebrich <samkoebrich@gmail.com>
2025-02-27 10:22:14 -0700

    make shapely an optional dependency for [testing]

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name="watttime",
     description="An officially maintained python client for WattTime's API providing access to electricity grid emissions data.",
-    long_description=open('README.md').read(),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     version="v1.2.2",
     packages=["watttime"],
     python_requires=">=3.8",
     install_requires=["requests", "pandas>1.0.0", "python-dateutil", "shapely"],
+    extras_require={"testing": ["pytest", "shapely", "flake8"]},
 )

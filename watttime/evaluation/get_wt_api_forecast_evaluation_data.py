32b25e3ca2c52274dbd0f4f3dcecd4efdba80a14
samkoebrich <samkoebrich@gmail.com>
2025-03-04 20:42:08 -0700

    add fuel_mix attribute to AnalysisDataHandler

--- watttime/evaluation/get_wt_api_forecast_evaluation_data.py
+++ watttime/evaluation/get_wt_api_forecast_evaluation_data.py
@@ -1,6 +1,7 @@
 from typing import Optional, List, Union
 from datetime import datetime
 import inspect
+import warnings
 import random
 import math
 from dateutil.parser import parse
@@ -12,6 +13,7 @@ from dataclasses import dataclass
 import pandas as pd
 
 from watttime import api
+from watttime import api_do_not_commit
 
 
 @dataclass
@@ -54,13 +56,18 @@ class AnalysisDataHandler:
     forecast_max_horizon: int = 60 * 24
     forecast_sample_seed: int = 42
     signal_type: str = "co2_moer"
-    model_date: Optional[
-        str
-    ] = None  # if None, will default to latest provided through API
+    model_date: Optional[str] = (
+        None  # if None, will default to latest provided through API
+    )
     wt_forecast: Optional[api.WattTimeForecast] = api.WattTimeForecast(
         multithreaded=True
     )
-    wt_hist: Optional[api.WattTimeHistorical] = api.WattTimeHistorical()
+    wt_hist: Optional[api.WattTimeHistorical] = api.WattTimeHistorical(
+        multithreaded=True
+    )
+    wt_fuel_mix: Optional[api_do_not_commit.WattTimeFuelMix] = (
+        api_do_not_commit.WattTimeFuelMix(multithreaded=False)
+    )
 
     def __post_init__(self):
         self.region = self.region.upper()
@@ -70,6 +77,7 @@ class AnalysisDataHandler:
             self.eval_start = parse(self.eval_start)
         if isinstance(self.eval_end, str):
             self.eval_end = parse(self.eval_end)
+        assert self.eval_start < self.eval_end
         self.eval_days = list(
             pd.date_range(start=self.eval_start, end=self.eval_end, freq="1D")
         )
@@ -106,25 +114,52 @@ class AnalysisDataHandler:
 
     @cached_property
     def forecasts(self) -> pd.DataFrame:
+        # context manager to only return critical logs
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            forecasts = self.wt_forecast.get_historical_forecast_pandas_list(
+                list_of_dates=self.sample_days,
+                region=self.region,
+                signal_type=self.signal_type,
+                model=self.model_date,
+                horizon_hours=math.ceil(self.forecast_max_horizon / 60),
+            )
 
-        forecasts = self.wt_forecast.get_historical_forecast_pandas_list(
-            list_of_dates=self.sample_days,
+            forecasts["point_time"] = pd.to_datetime(forecasts["point_time"])
+            forecasts["horizon_mins"] = (
+                forecasts["point_time"] - forecasts["generated_at"]
+            ).dt.total_seconds() / 60
+            forecasts.rename({"value": "predicted_value"}, axis="columns", inplace=True)
+            self.returned_meta = self.wt_forecast._last_request_meta
+            self.returned_forecast_model_date = self.wt_forecast._last_request_meta[
+                "model"
+            ]["date"]
+            return forecasts
+
+    @cached_property
+    def fuel_mix(self) -> pd.DataFrame:
+        fm = self.wt_fuel_mix.get_fuel_mix_pandas(
+            start=self.eval_start,
+            end=self.eval_end
+            + pd.Timedelta(
+                minutes=self.forecast_max_horizon
+            ),  # include truth_values for final generated_at
             region=self.region,
-            signal_type=self.signal_type,
+            signal_type="marginal_fuel_mix",
             model=self.model_date,
-            horizon_hours=math.ceil(self.forecast_max_horizon / 60),
         )
 
-        forecasts["point_time"] = pd.to_datetime(forecasts["point_time"])
-        forecasts["horizon_mins"] = (
-            forecasts["point_time"] - forecasts["generated_at"]
-        ).dt.total_seconds() / 60
-        forecasts.rename({"value": "predicted_value"}, axis="columns", inplace=True)
-        self.returned_meta = self.wt_forecast._last_request_meta
-        self.returned_forecast_model_date = self.wt_forecast._last_request_meta[
-            "model"
-        ]["date"]
-        return forecasts
+        self.returned_meta = self.wt_fuel_mix._last_request_meta
+        # self.returned_fuel_mix_model_date = self.wt_fuel_mix._last_request_meta["model"]["date"]
+        return fm
+
+    def moer_v_fuel_mix(self):
+        return self.moers.merge(
+            self.fuel_mix,
+            how="inner",
+            left_on="point_time",
+            right_on="point_time",
+        ).set_index(["generated_at", "point_time"], drop=True)
 
     @cached_property
     def forecasts_v_moers(self) -> pd.DataFrame:
@@ -156,7 +191,7 @@ class DataHandlerFactory:
         signal_types (List[str]): List of signal types to analyze
         model_dates (List[str]): List of model dates to analyze
     """
-    
+
     def __init__(
         self,
         eval_start: Union[str, datetime],

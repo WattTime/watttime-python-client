de8a13f633efad1a04a3af23587be56167430c21
samkoebrich <samkoebrich@gmail.com>
2025-03-14 12:22:09 -0600

    reduce max threadpool workers to appease low CPU runtimes (e.g. github test runner)

--- watttime/api.py
+++ watttime/api.py
@@ -293,7 +293,9 @@ class WattTimeBase:
 
         responses = []
         if self.multithreaded:
-            with ThreadPoolExecutor(max_workers=10) as executor:
+            with ThreadPoolExecutor(
+                max_workers=min(10, os.cpu_count() * 2)
+            ) as executor:
                 futures = {
                     executor.submit(
                         self._make_rate_limited_request, url, params
